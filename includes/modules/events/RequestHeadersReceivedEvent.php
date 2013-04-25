53e45ff74c2ff1ee37af580cbbc029914a89f842
Clay Freeman <git@clayfreeman.com>
2013-04-24 20:51:20 -0500

    Maybe this will work.

--- includes/modules/events/RequestHeadersReceivedEvent.php
+++ includes/modules/events/RequestHeadersReceivedEvent.php
@@ -7,45 +7,48 @@
 			$preprocessors = $registrations[1];
 			$registrations = $registrations[0];
 			
-			$data = trim($data);
+			$chunk = explode("\n", $data);
 			
-			if (!isset($this->metadata[$connection->getID()])) {
-				$this->metadata[$connection->getID()] = array();
-			}
+			foreach ($chunk as $data) {
+				$data = trim($data);
+				if (!isset($this->metadata[$connection->getID()])) {
+					$this->metadata[$connection->getID()] = array();
+				}
 			
-			if (!isset($this->metadata[$connection->getID()]['headerlines'])) {
-				$this->metadata[$connection->getID()]['headerlines'] = array();
-			}
+				if (!isset($this->metadata[$connection->getID()]['headerlines'])) {
+					$this->metadata[$connection->getID()]['headerlines'] = array();
+				}
 			
-			if ($data != null) {
-				$this->metadata[$connection->getID()]['headerlines'][] = $data;
-			}
-			else {
-				$headers = array();
-				foreach ($this->metadata[$connection->getID()]['headerlines'] as $id => $line) {
-					if (trim($line) == null) {
-						unset($this->metadata[$connection->getID()]['headerlines'][$id]);
-					}
-					else {
-						if (stristr($line, ":")) {
-							$val = explode(":", $line);
-							if (count($val) > 2) {
-								$tmp = $val[0];
-								unset($val[0]);
-								$val = array($tmp, implode(":", $val));
-								unset($tmp);
-							}
+				if ($data != null) {
+					$this->metadata[$connection->getID()]['headerlines'][] = $data;
+				}
+				else {
+					$headers = array();
+					foreach ($this->metadata[$connection->getID()]['headerlines'] as $id => $line) {
+						if (trim($line) == null) {
+							unset($this->metadata[$connection->getID()]['headerlines'][$id]);
+						}
+						else {
+							if (stristr($line, ":")) {
+								$val = explode(":", $line);
+								if (count($val) > 2) {
+									$tmp = $val[0];
+									unset($val[0]);
+									$val = array($tmp, implode(":", $val));
+									unset($tmp);
+								}
 							
-							$headers[strtolower(preg_replace("[^a-zA-Z0-9]", null, $val[0]))] = $val[1];
-							unset($val);
+								$headers[strtolower(preg_replace("[^a-zA-Z0-9]", null, $val[0]))] = $val[1];
+								unset($val);
+							}
 						}
 					}
-				}
 				
-				foreach ($registrations as $id => $registration) {
-					EventHandling::triggerEvent($name, $id, array($connection->getID(), $this->metadata[$connection->getID()]['headerlines'], $headers));
+					foreach ($registrations as $id => $registration) {
+						EventHandling::triggerEvent($name, $id, array($connection->getID(), $this->metadata[$connection->getID()]['headerlines'], $headers));
+					}
+					unset($this->metadata[$connection->getID()]);
 				}
-				unset($this->metadata[$connection->getID()]);
 			}
 		}
 		
