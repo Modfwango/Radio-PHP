5a40559ef9c0102ce6ac1ff7389823f8c2b2471e
Clay Freeman <git@clayfreeman.com>
2015-04-09 18:12:48 -0500

    Populate buffer regardless of client count.

--- modules/SHOUTcast/Buffer.php
+++ modules/SHOUTcast/Buffer.php
@@ -9,44 +9,37 @@
     private $welcome = null;
 
     public function receiveConnectionLoopEnd() {
-      if (count($this->stream->getClients()) > 0) {
-        // Check for data and process it if there is any
-        if (!@feof($this->pipes[1])) {
-          // Logger::debug("Reading MP3 data...");
-          $this->stream->putPool(@fread($this->pipes[1],
-            $this->welcome->getOption("burstint")));
-        }
-
-        if (!is_resource($this->process) || feof($this->pipes[1])) {
-          @proc_close($this->process);
-          $this->process = null;
-          $this->pipes = null;
-
-          // Switch song
-          $this->stream->nextSong();
-          $this->song = $this->stream->getSong();
-
-          $pipes = array(
-            0 => array("pipe", "r"),
-            1 => array("pipe", "w"),
-            2 => array("pipe", "a")
-          );
-          $cmd = "avconv -v quiet -i ".escapeshellarg($this->song)." -c ".
-            "libmp3lame -ar ".$this->welcome->getOption("samplerate")." -ab ".
-            $this->welcome->getOption("bitrate")."k -minrate ".
-            $this->welcome->getOption("bitrate")."k -maxrate ".
-            $this->welcome->getOption("bitrate")."k -f mp3 -";
-          Logger::debug($cmd);
-          $this->process = proc_open($cmd, $pipes, $this->pipes);
-          stream_set_blocking($this->pipes[0], 0);
-          stream_set_blocking($this->pipes[1], 0);
-          stream_set_blocking($this->pipes[2], 0);
-        }
+      // Check for data and process it if there is any
+      if (!@feof($this->pipes[1])) {
+        // Logger::debug("Reading MP3 data...");
+        $this->stream->putPool(@fread($this->pipes[1],
+          $this->welcome->getOption("burstint")));
       }
-      else {
+
+      if (!is_resource($this->process) || feof($this->pipes[1])) {
         @proc_close($this->process);
         $this->process = null;
         $this->pipes = null;
+
+        // Switch song
+        $this->stream->nextSong();
+        $this->song = $this->stream->getSong();
+
+        $pipes = array(
+          0 => array("pipe", "r"),
+          1 => array("pipe", "w"),
+          2 => array("pipe", "a")
+        );
+        $cmd = "avconv -v quiet -i ".escapeshellarg($this->song)." -c ".
+          "libmp3lame -ar ".$this->welcome->getOption("samplerate")." -ab ".
+          $this->welcome->getOption("bitrate")."k -minrate ".
+          $this->welcome->getOption("bitrate")."k -maxrate ".
+          $this->welcome->getOption("bitrate")."k -f mp3 -";
+        Logger::debug($cmd);
+        $this->process = proc_open($cmd, $pipes, $this->pipes);
+        stream_set_blocking($this->pipes[0], 0);
+        stream_set_blocking($this->pipes[1], 0);
+        stream_set_blocking($this->pipes[2], 0);
       }
     }
 
