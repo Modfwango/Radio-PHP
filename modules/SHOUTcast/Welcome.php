a59753b70cfeec64b1c1668d6335bdb111e8bc84
Clay Freeman <git@clayfreeman.com>
2015-08-23 14:45:57 -0500

    Reset rate.

--- modules/SHOUTcast/Welcome.php
+++ modules/SHOUTcast/Welcome.php
@@ -136,7 +136,7 @@
 
         // Calculate the burstint field
         $config["burstint"] = intval((($config["bitrate"] * 1000) / 8) *
-          $config["rate"] * 2);
+          $config["rate"]);
 
         // Accept the config as given
         $this->config = $config;
